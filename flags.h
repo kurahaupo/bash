037579bfca1fe8bf294afb58e5f2dc71dc561420
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-08-19 14:32:18 +1000

    Handle place_keywords_in_env (-k, keyword) within subst.c

--- flags.h
+++ flags.h
@@ -137,7 +137,7 @@ ValidateFlagOrError (char const *file, unsigned int line, char const *expr_str, 
 
 
 extern int
-  place_keywords_in_env, read_but_dont_execute,
+  read_but_dont_execute,
   just_one_command, unbound_vars_is_error, echo_input_at_read, verbose_flag,
   echo_command_at_execute, noclobber,
   privileged_mode, jobs_m_flag,
