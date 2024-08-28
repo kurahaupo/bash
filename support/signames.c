c7b48c1e62f17e00ae98a1cec21143ec1aa7685b
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-08-29 01:49:20 +1000

    Use static initialisers for (most) signal names

--- support/signames.c
+++ support/signames.c
@@ -44,8 +44,6 @@
  */
 #define LASTSIG NSIG+2
 
-char *signal_names[2 * (LASTSIG)];
-
 #define signal_names_size (sizeof(signal_names)/sizeof(signal_names[0]))
 
 /* AIX 4.3 defines SIGRTMIN and SIGRTMAX as 888 and 999 respectively.
@@ -62,6 +60,331 @@ char *signal_names[2 * (LASTSIG)];
 #  define RTLIM 256
 #endif
 
+char *signal_names[2 * (LASTSIG)] = {
+  "EXIT"
+
+#if defined (SIGLOST)	/* resource lost (eg, record-lock lost) */
+  , [SIGLOST] = "SIGLOST"
+#endif
+
+  /* AIX */
+#if defined (SIGMSG)	/* HFT input data pending */
+  , [SIGMSG] = "SIGMSG"
+#endif
+
+#if defined (SIGDANGER)	/* system crash imminent */
+  , [SIGDANGER] = "SIGDANGER"
+#endif
+
+#if defined (SIGMIGRATE) /* migrate process to another CPU */
+  , [SIGMIGRATE] = "SIGMIGRATE"
+#endif
+
+#if defined (SIGPRE)	/* programming error */
+  , [SIGPRE] = "SIGPRE"
+#endif
+
+#if defined (SIGPHONE)	/* Phone interrupt */
+  , [SIGPHONE] = "SIGPHONE"
+#endif
+
+#if defined (SIGVIRT)	/* AIX virtual time alarm */
+  , [SIGVIRT] = "SIGVIRT"
+#endif
+
+#if defined (SIGTINT)	/* Interrupt */
+  , [SIGTINT] = "SIGTINT"
+#endif
+
+#if defined (SIGALRM1)	/* m:n condition variables */
+  , [SIGALRM1] = "SIGALRM1"
+#endif
+
+#if defined (SIGWAITING)	/* m:n scheduling */
+  , [SIGWAITING] = "SIGWAITING"
+#endif
+
+#if defined (SIGGRANT)	/* HFT monitor mode granted */
+  , [SIGGRANT] = "SIGGRANT"
+#endif
+
+#if defined (SIGKAP)	/* keep alive poll from native keyboard */
+  , [SIGKAP] = "SIGKAP"
+#endif
+
+#if defined (SIGRETRACT) /* HFT monitor mode retracted */
+  , [SIGRETRACT] = "SIGRETRACT"
+#endif
+
+#if defined (SIGSOUND)	/* HFT sound sequence has completed */
+  , [SIGSOUND] = "SIGSOUND"
+#endif
+
+#if defined (SIGSAK)	/* Secure Attention Key */
+  , [SIGSAK] = "SIGSAK"
+#endif
+
+#if defined (SIGCPUFAIL)	/* Predictive processor deconfiguration */
+  , [SIGCPUFAIL] = "SIGCPUFAIL"
+#endif
+
+#if defined (SIGAIO)	/* Asynchronous I/O */
+  , [SIGAIO] = "SIGAIO"
+#endif
+
+#if defined (SIGLAB)	/* Security label changed */
+  , [SIGLAB] = "SIGLAB"
+#endif
+
+  /* SunOS5 */
+#if defined (SIGLWP)	/* Solaris: special signal used by thread library */
+  , [SIGLWP] = "SIGLWP"
+#endif
+
+#if defined (SIGFREEZE)	/* Solaris: special signal used by CPR */
+  , [SIGFREEZE] = "SIGFREEZE"
+#endif
+
+#if defined (SIGTHAW)	/* Solaris: special signal used by CPR */
+  , [SIGTHAW] = "SIGTHAW"
+#endif
+
+#if defined (SIGCANCEL)	/* Solaris: thread cancellation signal used by libthread */
+  , [SIGCANCEL] = "SIGCANCEL"
+#endif
+
+#if defined (SIGXRES)	/* Solaris: resource control exceeded */
+  , [SIGXRES] = "SIGXRES"
+#endif
+
+#if defined (SIGJVM1)	/* Solaris: Java Virtual Machine 1 */
+  , [SIGJVM1] = "SIGJVM1"
+#endif
+
+#if defined (SIGJVM2)	/* Solaris: Java Virtual Machine 2 */
+  , [SIGJVM2] = "SIGJVM2"
+#endif
+
+#if defined (SIGDGTIMER1)
+  , [SIGDGTIMER1] = "SIGDGTIMER1"
+#endif
+
+#if defined (SIGDGTIMER2)
+  , [SIGDGTIMER2] = "SIGDGTIMER2"
+#endif
+
+#if defined (SIGDGTIMER3)
+  , [SIGDGTIMER3] = "SIGDGTIMER3"
+#endif
+
+#if defined (SIGDGTIMER4)
+  , [SIGDGTIMER4] = "SIGDGTIMER4"
+#endif
+
+#if defined (SIGDGNOTIFY)
+  , [SIGDGNOTIFY] = "SIGDGNOTIFY"
+#endif
+
+  /* Apollo */
+#if defined (SIGAPOLLO)
+  , [SIGAPOLLO] = "SIGAPOLLO"
+#endif
+
+  /* HP-UX */
+#if defined (SIGDIL)	/* DIL signal (?) */
+  , [SIGDIL] = "SIGDIL"
+#endif
+
+  /* System V */
+#if defined (SIGCLD)	/* Like SIGCHLD.  */
+  , [SIGCLD] = "SIGCLD"
+#endif
+
+#if defined (SIGPWR)	/* power state indication */
+  , [SIGPWR] = "SIGPWR"
+#endif
+
+#if defined (SIGPOLL)	/* Pollable event (for streams)  */
+  , [SIGPOLL] = "SIGPOLL"
+#endif
+
+  /* Unknown */
+#if defined (SIGWINDOW)
+  , [SIGWINDOW] = "SIGWINDOW"
+#endif
+
+  /* Linux */
+#if defined (SIGSTKFLT)
+  , [SIGSTKFLT] = "SIGSTKFLT"
+#endif
+
+  /* FreeBSD */
+#if defined (SIGTHR)	/* thread interrupt */
+  , [SIGTHR] = "SIGTHR"
+#endif
+
+  /* z/OS */
+#if defined (SIGABND)
+  , [SIGABND] = "SIGABND"
+#endif
+#if defined (SIGIOERR)
+  , [SIGIOERR] = "SIGIOERR"
+#endif
+#if defined (SIGTHSTOP)
+  , [SIGTHSTOP] = "SIGTHSTOP"
+#endif
+#if defined (SIGTHCONT)
+  , [SIGTHCONT] = "SIGTHCONT"
+#endif
+#if defined (SIGTRACE)
+  , [SIGTRACE] = "SIGTRACE"
+#endif
+#if defined (SIGDCE)
+  , [SIGDCE] = "SIGDCE"
+#endif
+#if defined (SIGDUMP)
+  , [SIGDUMP] = "SIGDUMP"
+#endif
+
+  /* Common */
+#if defined (SIGHUP)	/* hangup */
+  , [SIGHUP] = "SIGHUP"
+#endif
+
+#if defined (SIGINT)	/* interrupt */
+  , [SIGINT] = "SIGINT"
+#endif
+
+#if defined (SIGQUIT)	/* quit */
+  , [SIGQUIT] = "SIGQUIT"
+#endif
+
+#if defined (SIGILL)	/* illegal instruction (not reset when caught) */
+  , [SIGILL] = "SIGILL"
+#endif
+
+#if defined (SIGTRAP)	/* trace trap (not reset when caught) */
+  , [SIGTRAP] = "SIGTRAP"
+#endif
+
+#if defined (SIGIOT)	/* IOT instruction */
+  , [SIGIOT] = "SIGIOT"
+#endif
+
+#if defined (SIGABRT)	/* Cause current process to dump core. */
+  , [SIGABRT] = "SIGABRT"
+#endif
+
+#if defined (SIGEMT)	/* EMT instruction */
+  , [SIGEMT] = "SIGEMT"
+#endif
+
+#if defined (SIGFPE)	/* floating point exception */
+  , [SIGFPE] = "SIGFPE"
+#endif
+
+#if defined (SIGKILL)	/* kill (cannot be caught or ignored) */
+  , [SIGKILL] = "SIGKILL"
+#endif
+
+#if defined (SIGBUS)	/* bus error */
+  , [SIGBUS] = "SIGBUS"
+#endif
+
+#if defined (SIGSEGV)	/* segmentation violation */
+  , [SIGSEGV] = "SIGSEGV"
+#endif
+
+#if defined (SIGSYS)	/* bad argument to system call */
+  , [SIGSYS] = "SIGSYS"
+#endif
+
+#if defined (SIGPIPE)	/* write on a pipe with no one to read it */
+  , [SIGPIPE] = "SIGPIPE"
+#endif
+
+#if defined (SIGALRM)	/* alarm clock */
+  , [SIGALRM] = "SIGALRM"
+#endif
+
+#if defined (SIGTERM)	/* software termination signal from kill */
+  , [SIGTERM] = "SIGTERM"
+#endif
+
+#if defined (SIGURG)	/* urgent condition on IO channel */
+  , [SIGURG] = "SIGURG"
+#endif
+
+#if defined (SIGSTOP)	/* sendable stop signal not from tty */
+  , [SIGSTOP] = "SIGSTOP"
+#endif
+
+#if defined (SIGTSTP)	/* stop signal from tty */
+  , [SIGTSTP] = "SIGTSTP"
+#endif
+
+#if defined (SIGCONT)	/* continue a stopped process */
+  , [SIGCONT] = "SIGCONT"
+#endif
+
+#if defined (SIGCHLD)	/* to parent on child stop or exit */
+  , [SIGCHLD] = "SIGCHLD"
+#endif
+
+#if defined (SIGTTIN)	/* to readers pgrp upon background tty read */
+  , [SIGTTIN] = "SIGTTIN"
+#endif
+
+#if defined (SIGTTOU)	/* like TTIN for output if (tp->t_local&LTOSTOP) */
+  , [SIGTTOU] = "SIGTTOU"
+#endif
+
+#if defined (SIGIO)	/* input/output possible signal */
+  , [SIGIO] = "SIGIO"
+#endif
+
+#if defined (SIGXCPU)	/* exceeded CPU time limit */
+  , [SIGXCPU] = "SIGXCPU"
+#endif
+
+#if defined (SIGXFSZ)	/* exceeded file size limit */
+  , [SIGXFSZ] = "SIGXFSZ"
+#endif
+
+#if defined (SIGVTALRM)	/* virtual time alarm */
+  , [SIGVTALRM] = "SIGVTALRM"
+#endif
+
+#if defined (SIGPROF)	/* profiling time alarm */
+  , [SIGPROF] = "SIGPROF"
+#endif
+
+#if defined (SIGWINCH)	/* window changed */
+  , [SIGWINCH] = "SIGWINCH"
+#endif
+
+  /* 4.4 BSD */
+#if defined (SIGINFO) && !defined _SEQUENT_	/* information request */
+  , [SIGINFO] = "SIGINFO"
+#endif
+
+#if defined (SIGUSR1)	/* user defined signal 1 */
+  , [SIGUSR1] = "SIGUSR1"
+#endif
+
+#if defined (SIGUSR2)	/* user defined signal 2 */
+  , [SIGUSR2] = "SIGUSR2"
+#endif
+
+#if defined (SIGKILLTHR)	/* BeOS: Kill Thread */
+  , [SIGKILLTHR] = "SIGKILLTHR"
+#endif
+
+  , [NSIG] = "DEBUG"
+  , [NSIG+1] = "ERR"
+  , [NSIG+2] = "RETURN"
+};
+
 #if defined (BUILDTOOL)
 extern char *progname;
 #endif
@@ -69,17 +392,6 @@ extern char *progname;
 void
 initialize_signames (void)
 {
-  register int i;
-#if defined (SIGRTMAX) || defined (SIGRTMIN)
-  int rtmin, rtmax, rtcnt;
-#endif
-
-  for (i = 1; i < signal_names_size; i++)
-    signal_names[i] = (char *)NULL;
-
-  /* `signal' 0 is what we do on exit. */
-  signal_names[0] = "EXIT";
-
   /* Place signal names which can be aliases for more common signal
      names first.  This allows (for example) SIGABRT to overwrite SIGLOST. */
 
@@ -94,376 +406,57 @@ initialize_signames (void)
      of RT signals is odd, there is an extra SIGRTMIN+(x+1).
      These names are the ones used by ksh and /usr/xpg4/bin/sh on SunOS5. */
 
-#if defined (SIGRTMIN)
-  rtmin = SIGRTMIN;
-  signal_names[rtmin] = "SIGRTMIN";
-#endif
-
 #if defined (SIGRTMAX)
-  rtmax = SIGRTMAX;
-  signal_names[rtmax] = "SIGRTMAX";
-#endif
+  signal_names[SIGRTMAX] = "SIGRTMAX";
+# if defined (SIGRTMIN)
+  signal_names[SIGRTMIN] = "SIGRTMIN";
 
-#if defined (SIGRTMAX) && defined (SIGRTMIN)
+  const int rtmax = SIGRTMAX;
+  const int rtmin = SIGRTMIN;
+
   if (rtmax > rtmin)
     {
-      rtcnt = (rtmax - rtmin - 1) / 2;
-      /* croak if there are too many RT signals */
-      if (rtcnt >= RTLIM/2)
-	{
-	  rtcnt = RTLIM/2-1;
-#ifdef BUILDTOOL
-	  fprintf(stderr, "%s: error: more than %d real time signals, fix `%s'\n",
-		  progname, RTLIM, progname);
-#endif
-	}
+      const int rt_raw_count = rtmax - rtmin + 1;
+#  if defined RTLIM
+#   if defined BUILDTOOL
+    if (RTLIM < SIGRTMAX - SIGRTMIN + 1)
+      /* complain if there are too many RT signals */
+      fprintf(stderr, "%s: error: more than %d real time signals, fix `%s'\n",
+	      progname, RTLIM, progname);
+#   endif
+      const int rt_count = RTLIM < rt_raw_count
+                         ? RTLIM : rt_raw_count;
+#  else
+      const int rt_count = rt_raw_count;
+#  endif
+      /* Count half up from RTMIN and half down from RTMAX.
+         Round up in case the total count is odd.
+      */
+      const int rt_half_count = (rt_count+1) / 2;
 
-      for (i = 1; i <= rtcnt; i++)
-	{
-	  signal_names[rtmin+i] = (char *)malloc(RTLEN);
-	  if (signal_names[rtmin+i])
-	    sprintf (signal_names[rtmin+i], "SIGRTMIN+%d", i);
-	  signal_names[rtmax-i] = (char *)malloc(RTLEN);
-	  if (signal_names[rtmax-i])
-	    sprintf (signal_names[rtmax-i], "SIGRTMAX-%d", i);
-	}
+      char **minp = signal_names + rtmin;
+      char **p = minp + 1;
 
-      if (rtcnt < RTLIM/2-1 && rtcnt != (rtmax-rtmin)/2)
-	{
-	  /* Need an extra RTMIN signal */
-	  signal_names[rtmin+rtcnt+1] = (char *)malloc(RTLEN);
-	  if (signal_names[rtmin+rtcnt+1])
-	    sprintf (signal_names[rtmin+rtcnt+1], "SIGRTMIN+%d", rtcnt+1);
-	}
+      for (char **midp = signal_names + rtmin + rt_half_count; p < midp ; ++p)
+	if (*p == NULL)
+	  if (asprintf(p, "SIGRTMIN%+zd", p - minp) < 0)
+	    *p = NULL;
+
+      p += rt_raw_count - rt_count; /* Leave a gap if there's too many */
+
+      for (char **maxp = signal_names + rtmax; p < maxp ; ++p)
+	if (*p == NULL)
+	  if (asprintf(p, "SIGRTMAX%+zd", p - maxp) < 0)
+	    *p = NULL;
     }
-#endif /* SIGRTMIN && SIGRTMAX */
-
-#if defined (SIGLOST)	/* resource lost (eg, record-lock lost) */
-  signal_names[SIGLOST] = "SIGLOST";
-#endif
-
-/* AIX */
-#if defined (SIGMSG)	/* HFT input data pending */
-  signal_names[SIGMSG] = "SIGMSG";
-#endif
-
-#if defined (SIGDANGER)	/* system crash imminent */
-  signal_names[SIGDANGER] = "SIGDANGER";
-#endif
-
-#if defined (SIGMIGRATE) /* migrate process to another CPU */
-  signal_names[SIGMIGRATE] = "SIGMIGRATE";
-#endif
-
-#if defined (SIGPRE)	/* programming error */
-  signal_names[SIGPRE] = "SIGPRE";
-#endif
-
-#if defined (SIGPHONE)	/* Phone interrupt */
-  signal_names[SIGPHONE] = "SIGPHONE";
-#endif
-
-#if defined (SIGVIRT)	/* AIX virtual time alarm */
-  signal_names[SIGVIRT] = "SIGVIRT";
-#endif
-
-#if defined (SIGTINT)	/* Interrupt */
-  signal_names[SIGTINT] = "SIGTINT";
-#endif
-
-#if defined (SIGALRM1)	/* m:n condition variables */
-  signal_names[SIGALRM1] = "SIGALRM1";
-#endif
-
-#if defined (SIGWAITING)	/* m:n scheduling */
-  signal_names[SIGWAITING] = "SIGWAITING";
-#endif
-
-#if defined (SIGGRANT)	/* HFT monitor mode granted */
-  signal_names[SIGGRANT] = "SIGGRANT";
-#endif
-
-#if defined (SIGKAP)	/* keep alive poll from native keyboard */
-  signal_names[SIGKAP] = "SIGKAP";
-#endif
-
-#if defined (SIGRETRACT) /* HFT monitor mode retracted */
-  signal_names[SIGRETRACT] = "SIGRETRACT";
-#endif
-
-#if defined (SIGSOUND)	/* HFT sound sequence has completed */
-  signal_names[SIGSOUND] = "SIGSOUND";
-#endif
-
-#if defined (SIGSAK)	/* Secure Attention Key */
-  signal_names[SIGSAK] = "SIGSAK";
-#endif
-
-#if defined (SIGCPUFAIL)	/* Predictive processor deconfiguration */
-  signal_names[SIGCPUFAIL] = "SIGCPUFAIL";
-#endif
-
-#if defined (SIGAIO)	/* Asynchronous I/O */
-  signal_names[SIGAIO] = "SIGAIO";
-#endif
-
-#if defined (SIGLAB)	/* Security label changed */
-  signal_names[SIGLAB] = "SIGLAB";
-#endif
-
-/* SunOS5 */
-#if defined (SIGLWP)	/* Solaris: special signal used by thread library */
-  signal_names[SIGLWP] = "SIGLWP";
-#endif
-
-#if defined (SIGFREEZE)	/* Solaris: special signal used by CPR */
-  signal_names[SIGFREEZE] = "SIGFREEZE";
-#endif
-
-#if defined (SIGTHAW)	/* Solaris: special signal used by CPR */
-  signal_names[SIGTHAW] = "SIGTHAW";
-#endif
-
-#if defined (SIGCANCEL)	/* Solaris: thread cancellation signal used by libthread */
-  signal_names[SIGCANCEL] = "SIGCANCEL";
-#endif
-
-#if defined (SIGXRES)	/* Solaris: resource control exceeded */
-  signal_names[SIGXRES] = "SIGXRES";
-#endif
-
-#if defined (SIGJVM1)	/* Solaris: Java Virtual Machine 1 */
-  signal_names[SIGJVM1] = "SIGJVM1";
-#endif
-
-#if defined (SIGJVM2)	/* Solaris: Java Virtual Machine 2 */
-  signal_names[SIGJVM2] = "SIGJVM2";
-#endif
-
-#if defined (SIGDGTIMER1)
-  signal_names[SIGDGTIMER1] = "SIGDGTIMER1";
-#endif
-
-#if defined (SIGDGTIMER2)
-  signal_names[SIGDGTIMER2] = "SIGDGTIMER2";
-#endif
-
-#if defined (SIGDGTIMER3)
-  signal_names[SIGDGTIMER3] = "SIGDGTIMER3";
-#endif
-
-#if defined (SIGDGTIMER4)
-  signal_names[SIGDGTIMER4] = "SIGDGTIMER4";
-#endif
-
-#if defined (SIGDGNOTIFY)
-  signal_names[SIGDGNOTIFY] = "SIGDGNOTIFY";
-#endif
-
-/* Apollo */
-#if defined (SIGAPOLLO)
-  signal_names[SIGAPOLLO] = "SIGAPOLLO";
-#endif
-
-/* HP-UX */
-#if defined (SIGDIL)	/* DIL signal (?) */
-  signal_names[SIGDIL] = "SIGDIL";
-#endif
-
-/* System V */
-#if defined (SIGCLD)	/* Like SIGCHLD.  */
-  signal_names[SIGCLD] = "SIGCLD";
-#endif
-
-#if defined (SIGPWR)	/* power state indication */
-  signal_names[SIGPWR] = "SIGPWR";
-#endif
-
-#if defined (SIGPOLL)	/* Pollable event (for streams)  */
-  signal_names[SIGPOLL] = "SIGPOLL";
-#endif
-
-/* Unknown */
-#if defined (SIGWINDOW)
-  signal_names[SIGWINDOW] = "SIGWINDOW";
-#endif
-
-/* Linux */
-#if defined (SIGSTKFLT)
-  signal_names[SIGSTKFLT] = "SIGSTKFLT";
-#endif
-
-/* FreeBSD */
-#if defined (SIGTHR)	/* thread interrupt */
-  signal_names[SIGTHR] = "SIGTHR";
-#endif
-
-/* z/OS */
-#if defined(SIGABND)
-  signal_names[SIGABND] = "SIGABND";
-#endif
-#if defined(SIGIOERR)
-  signal_names[SIGIOERR] = "SIGIOERR";
-#endif
-#if defined(SIGTHSTOP)
-  signal_names[SIGTHSTOP] = "SIGTHSTOP";
-#endif
-#if defined(SIGTHCONT)
-  signal_names[SIGTHCONT] = "SIGTHCONT";
-#endif
-#if defined(SIGTRACE)
-  signal_names[SIGTRACE] = "SIGTRACE";
-#endif
-#if defined(SIGDCE)
-  signal_names[SIGDCE] = "SIGDCE";
-#endif
-#if defined(SIGDUMP)
-  signal_names[SIGDUMP] = "SIGDUMP";
-#endif
-
-/* Common */
-#if defined (SIGHUP)	/* hangup */
-  signal_names[SIGHUP] = "SIGHUP";
-#endif
-
-#if defined (SIGINT)	/* interrupt */
-  signal_names[SIGINT] = "SIGINT";
-#endif
-
-#if defined (SIGQUIT)	/* quit */
-  signal_names[SIGQUIT] = "SIGQUIT";
-#endif
-
-#if defined (SIGILL)	/* illegal instruction (not reset when caught) */
-  signal_names[SIGILL] = "SIGILL";
-#endif
-
-#if defined (SIGTRAP)	/* trace trap (not reset when caught) */
-  signal_names[SIGTRAP] = "SIGTRAP";
-#endif
-
-#if defined (SIGIOT)	/* IOT instruction */
-  signal_names[SIGIOT] = "SIGIOT";
-#endif
-
-#if defined (SIGABRT)	/* Cause current process to dump core. */
-  signal_names[SIGABRT] = "SIGABRT";
-#endif
-
-#if defined (SIGEMT)	/* EMT instruction */
-  signal_names[SIGEMT] = "SIGEMT";
-#endif
-
-#if defined (SIGFPE)	/* floating point exception */
-  signal_names[SIGFPE] = "SIGFPE";
-#endif
-
-#if defined (SIGKILL)	/* kill (cannot be caught or ignored) */
-  signal_names[SIGKILL] = "SIGKILL";
-#endif
-
-#if defined (SIGBUS)	/* bus error */
-  signal_names[SIGBUS] = "SIGBUS";
-#endif
-
-#if defined (SIGSEGV)	/* segmentation violation */
-  signal_names[SIGSEGV] = "SIGSEGV";
-#endif
-
-#if defined (SIGSYS)	/* bad argument to system call */
-  signal_names[SIGSYS] = "SIGSYS";
-#endif
-
-#if defined (SIGPIPE)	/* write on a pipe with no one to read it */
-  signal_names[SIGPIPE] = "SIGPIPE";
-#endif
-
-#if defined (SIGALRM)	/* alarm clock */
-  signal_names[SIGALRM] = "SIGALRM";
-#endif
-
-#if defined (SIGTERM)	/* software termination signal from kill */
-  signal_names[SIGTERM] = "SIGTERM";
-#endif
-
-#if defined (SIGURG)	/* urgent condition on IO channel */
-  signal_names[SIGURG] = "SIGURG";
-#endif
-
-#if defined (SIGSTOP)	/* sendable stop signal not from tty */
-  signal_names[SIGSTOP] = "SIGSTOP";
-#endif
-
-#if defined (SIGTSTP)	/* stop signal from tty */
-  signal_names[SIGTSTP] = "SIGTSTP";
-#endif
-
-#if defined (SIGCONT)	/* continue a stopped process */
-  signal_names[SIGCONT] = "SIGCONT";
-#endif
-
-#if defined (SIGCHLD)	/* to parent on child stop or exit */
-  signal_names[SIGCHLD] = "SIGCHLD";
-#endif
-
-#if defined (SIGTTIN)	/* to readers pgrp upon background tty read */
-  signal_names[SIGTTIN] = "SIGTTIN";
-#endif
-
-#if defined (SIGTTOU)	/* like TTIN for output if (tp->t_local&LTOSTOP) */
-  signal_names[SIGTTOU] = "SIGTTOU";
-#endif
-
-#if defined (SIGIO)	/* input/output possible signal */
-  signal_names[SIGIO] = "SIGIO";
-#endif
-
-#if defined (SIGXCPU)	/* exceeded CPU time limit */
-  signal_names[SIGXCPU] = "SIGXCPU";
-#endif
-
-#if defined (SIGXFSZ)	/* exceeded file size limit */
-  signal_names[SIGXFSZ] = "SIGXFSZ";
-#endif
-
-#if defined (SIGVTALRM)	/* virtual time alarm */
-  signal_names[SIGVTALRM] = "SIGVTALRM";
-#endif
-
-#if defined (SIGPROF)	/* profiling time alarm */
-  signal_names[SIGPROF] = "SIGPROF";
-#endif
-
-#if defined (SIGWINCH)	/* window changed */
-  signal_names[SIGWINCH] = "SIGWINCH";
-#endif
-
-/* 4.4 BSD */
-#if defined (SIGINFO) && !defined (_SEQUENT_)	/* information request */
-  signal_names[SIGINFO] = "SIGINFO";
-#endif
-
-#if defined (SIGUSR1)	/* user defined signal 1 */
-  signal_names[SIGUSR1] = "SIGUSR1";
-#endif
-
-#if defined (SIGUSR2)	/* user defined signal 2 */
-  signal_names[SIGUSR2] = "SIGUSR2";
-#endif
-
-#if defined (SIGKILLTHR)	/* BeOS: Kill Thread */
-  signal_names[SIGKILLTHR] = "SIGKILLTHR";
+# endif
+#elif defined (SIGRTMIN)
+  signal_names[SIGRTMIN] = "SIGRTMIN";
 #endif
 
-  for (i = 0; i < NSIG; i++)
-    if (signal_names[i] == (char *)NULL)
-      {
-	signal_names[i] = (char *)malloc (18);
-	if (signal_names[i])
-	  sprintf (signal_names[i], "SIGJUNK(%d)", i);
-      }
+  for (char **p = signal_names ; p<signal_names+NSIG; ++p)
+    if (*p == NULL)
+      if (asprintf (p, "SIGJUNK(%zd)", p - signal_names) < 0)
+	*p = "SIGJUNK";
 
-  signal_names[NSIG] = "DEBUG";
-  signal_names[NSIG+1] = "ERR";
-  signal_names[NSIG+2] = "RETURN";
 }
