31a545d3a3d1fc6b5f39f1c6621dfbad9656e050
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-09-08 16:31:55 +1000

    New getopt iterator for WORD_LIST

--- builtins/bashgetopt.h
+++ builtins/bashgetopt.h
@@ -40,4 +40,113 @@ extern WORD_LIST *loptend;
 extern int internal_getopt (WORD_LIST *, char *);
 extern void reset_internal_getopt (void);
 
+typedef enum getopt_arg_state_e {
+  GO_INVALID,
+  GO_VALID,
+  GO_NEED_ARG,
+  GO_OPT_ARG,
+} getopt_arg_state_t;
+
+#ifdef NOT_ASCII
+#define MIN_GRAPH 0
+#define MAX_GRAPH UCHAR_MAX
+#else
+#define MIN_GRAPH '!'
+#define MAX_GRAPH '~'
+#endif
+
+typedef struct getopt_parser_s {
+  _Bool lead_minus_minus :1;
+  _Bool lead_plus :1;
+  _Bool stop_on_solo_minus :1;
+  _Bool stop_on_solo_minus_minus :1;
+  unsigned char opts[MAX_GRAPH+1-MIN_GRAPH];
+} getopt_parser_t;
+
+static inline getopt_parser_t
+getopt_parser_start(char const *opts)
+{
+  getopt_parser_t P = {
+    .lead_minus = true,
+  };
+  for (; *opts;)
+    {
+      unsigned char c = *opts++;
+      if (c == '+')
+	P.lead_plus = true; 
+      else if (c == '-')
+	P.lead_minus_minus = false; 
+      else
+	{
+	  c -= MIN_GRAPH;
+	  if (c >= sizeof P.opts)
+	    abort();
+	  unsigned char *p = &P.opts[c];
+	  if (*opts == ':')
+	    ++opts, *p = GO_OPT_NEEDED;
+	  else if (*opts == ';')
+	    ++opts, *p = GO_OPT_ARG;
+	  else
+	    *p = GO_VALID;
+	}
+    }
+  return P;
+}
+
+typedef struct getopt_wl_iterator_s {
+  WORD_LIST const **curr;
+  size_t next_in_word;
+  getopt_parser_t *parser;
+  unsigned char opt;
+  const char * arg;
+  _Bool error :1;
+  _Bool force_stop :1;
+} getopt_wl_iterator_t;
+
+static inline getopt_wl_iterator_t
+getopt_wl_start(getopt_parser_t const *parser, WORD_LIST const **list)
+{
+  return (getopt_wl_iterator_t) {
+    .curr = list,
+    .next_in_word = 0,
+    .parser = parser,
+  };
+}
+
+static inline getopt_wl_iterator_t
+getopt_wlnc_start(char const *opts, WORD_LIST **list)
+{
+  return getopt_wl_start(opts, (WORD_LIST const **)list);
+}
+
+static inline _Bool
+getopt_wl_check(getopt_wl_iterator_t *I)
+{
+  if (!( I->curr && I->curr[0] && !I->force_stop ))
+    return false;
+
+  if (I->next_in_word == 0)
+    {
+      ;
+    }
+  unsigned char c = I->curr[0]->word->word[I->next_in_word];
+  c -= MIN_GRAPH;
+    {
+    }
+  return true;
+}
+
+#define for_each_wlist_getopt(opts, list, I) \
+	for (getopt_parser_t P = getopt_parser_start (opts), \
+	     getopt_wl_iterator_t I = getopt_wl_start(&P, &(list)) \
+	    ; getopt_wl_check(I) \
+	    ;)
+
+/* Use the following when the list is WORD_LIST *w rather than WORD_LIST const*w */
+#define for_each_wlist_getopt_nc(opts, list, I) \
+	for (getopt_parser_t P = getopt_parser_start (opts), \
+	     getopt_wl_iterator_t I = getopt_wlnc_start(&P, &(list)) \
+	    ; getopt_wl_check(I) \
+	    ;)
+
 #endif /* !__BASH_GETOPT_H */
