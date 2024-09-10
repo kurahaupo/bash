ec7d929b87e59bd3f0954e563504af23e2dee7cb
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-09-10 15:32:02 +1000

    Kill minus_o_option_value

--- builtins/common.h
+++ builtins/common.h
@@ -166,7 +166,6 @@ extern int read_builtin_timeout (int);
 extern void check_read_timeout (void);
 
 /* Functions from set.def */
-extern int minus_o_option_value (const char *);
 extern void list_minus_o_opts (int, int);
 extern char **get_minus_o_opts (void);
 extern int set_minus_o_option (char, char const *);
