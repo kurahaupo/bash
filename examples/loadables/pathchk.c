7e087185fa8a68faaafb76aab38ebf9e3c1aa140
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-07 21:41:52 +1000

    Reindent examples/loadables/pathchk.c

--- examples/loadables/pathchk.c
+++ examples/loadables/pathchk.c
@@ -111,7 +111,7 @@ pathchk_builtin (WORD_LIST *list)
 	case 'p':
 	  pflag = 1;
 	  break;
-	CASE_HELPOPT;
+	  CASE_HELPOPT;
 	default:
 	  builtin_usage ();
 	  return (EX_USAGE);
@@ -132,24 +132,24 @@ pathchk_builtin (WORD_LIST *list)
 }
 
 char *pathchk_doc[] = {
-	"Check pathnames for validity.",
-	"",
-	"Check each pathname argument for validity (i.e., it may be used to",
-	"create or access a file without causing syntax errors) and portability",
-	"(i.e., no filename truncation will result).  If the `-p' option is",
-	"supplied, more extensive portability checks are performed.",
-	(char *)NULL
+  "Check pathnames for validity.",
+  "",
+  "Check each pathname argument for validity (i.e., it may be used to",
+  "create or access a file without causing syntax errors) and portability",
+  "(i.e., no filename truncation will result).  If the `-p' option is",
+  "supplied, more extensive portability checks are performed.",
+  (char *)NULL
 };
 
 /* The standard structure describing a builtin command.  bash keeps an array
    of these structures. */
 struct builtin pathchk_struct = {
-	"pathchk",		/* builtin name */
-	pathchk_builtin,	/* function implementing the builtin */
-	BUILTIN_ENABLED,	/* initial flags for builtin */
-	pathchk_doc,		/* array of long documentation strings. */
-	"pathchk [-p] pathname ...",	/* usage synopsis */
-	0			/* reserved for internal use */
+  "pathchk",			/* builtin name */
+  pathchk_builtin,		/* function implementing the builtin */
+  BUILTIN_ENABLED,		/* initial flags for builtin */
+  pathchk_doc,			/* array of long documentation strings. */
+  "pathchk [-p] pathname ...",	/* usage synopsis */
+  0				/* reserved for internal use */
 };
 
 /* The remainder of this file is stolen shamelessly from `pathchk.c' in
@@ -161,16 +161,15 @@ struct builtin pathchk_struct = {
 /* Each element is nonzero if the corresponding ASCII character is
    in the POSIX portable character set, and zero if it is not.
    In addition, the entry for `/' is nonzero to simplify checking. */
-static char const portable_chars[256] =
-{
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 0-15 */
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* 16-31 */
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, /* 32-47 */
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, /* 48-63 */
-  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, /* 64-79 */
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, /* 80-95 */
-  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, /* 96-111 */
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, /* 112-127 */
+static char const portable_chars[256] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,	/* 0-15 */
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,	/* 16-31 */
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,	/* 32-47 */
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,	/* 48-63 */
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,	/* 64-79 */
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,	/* 80-95 */
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,	/* 96-111 */
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,	/* 112-127 */
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -189,7 +188,7 @@ portable_chars_only (const char *path)
   const char *p;
 
   for (p = path; *p; ++p)
-    if (portable_chars[(const unsigned char) *p] == 0)
+    if (portable_chars[(const unsigned char)*p] == 0)
       {
 	builtin_error ("path `%s' contains nonportable character `%c'", path, *p);
 	return 0;
@@ -200,9 +199,9 @@ portable_chars_only (const char *path)
 /* On some systems, stat can return EINTR.  */
 
 #ifndef EINTR
-# define SAFE_STAT(name, buf) stat (name, buf)
+#  define SAFE_STAT(name, buf) stat (name, buf)
 #else
-# define SAFE_STAT(name, buf) safe_stat (name, buf)
+#  define SAFE_STAT(name, buf) safe_stat (name, buf)
 static inline int
 safe_stat (const char *name, struct stat *buf)
 {
@@ -301,7 +300,7 @@ validate_path (char *path, int portability)
       char *start;		/* Start of path element being checked. */
 
       /* Find the end of this element of the path.
-	 Then chop off the rest of the path after this element. */
+         Then chop off the rest of the path after this element. */
       while (*slash == '/')
 	slash++;
       start = slash;
@@ -326,20 +325,20 @@ validate_path (char *path, int portability)
 
       length = slash - start;
       /* Since we know that `parent' is a directory, it's ok to call
-	 pathconf with it as the argument.  (If `parent' isn't a directory
-	 or doesn't exist, the behavior of pathconf is undefined.)
-	 But if `parent' is a directory and is on a remote file system,
-	 it's likely that pathconf can't give us a reasonable value
-	 and will return -1.  (NFS and tempfs are not POSIX . . .)
-	 In that case, we have no choice but to assume the pessimal
-	 POSIX minimums.  */
+         pathconf with it as the argument.  (If `parent' isn't a directory
+         or doesn't exist, the behavior of pathconf is undefined.)
+         But if `parent' is a directory and is on a remote file system,
+         it's likely that pathconf can't give us a reasonable value
+         and will return -1.  (NFS and tempfs are not POSIX . . .)
+         In that case, we have no choice but to assume the pessimal
+         POSIX minimums.  */
       name_max = portability ? _POSIX_NAME_MAX : NAME_MAX_FOR (parent);
       if (name_max < 0)
 	name_max = _POSIX_NAME_MAX;
       if (length > name_max)
 	{
 	  builtin_error ("name `%s' has length %d; exceeds limit of %d",
-		 start, length, name_max);
+			 start, length, name_max);
 	  free (parent);
 	  return 1;
 	}
@@ -365,7 +364,7 @@ validate_path (char *path, int portability)
   if (strlen (path) > path_max)
     {
       builtin_error ("path `%s' has length %lu; exceeds limit of %d",
-	     path, (unsigned long)strlen (path), path_max);
+		     path, (unsigned long)strlen (path), path_max);
       return 1;
     }
 
