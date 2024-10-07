470e744cb7f5fa8d01dfd1e4eee70952faa45100
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-07 21:00:21 +1000

    Tabify indentation in trap.c

--- trap.c
+++ trap.c
@@ -618,7 +618,7 @@ trap_handler (int sig)
 #else
       if (RL_ISSTATE (RL_STATE_SIGHANDLER) || RL_ISSTATE (RL_STATE_TERMPREPPED))
 #endif
-        bashline_set_event_hook ();
+	bashline_set_event_hook ();
 #endif
 
       errno = oerrno;
@@ -858,7 +858,7 @@ set_signal (int sig, const char *string)
     {
       /* If we aren't sure of the original value, check it. */
       if (original_signals[sig] == IMPOSSIBLE_TRAP_HANDLER)
-        GETORIGSIG (sig);
+	GETORIGSIG (sig);
       if (original_signals[sig] == SIG_IGN && (sigmodes[sig] & SIG_ASYNCSIG) == 0)
 	return;			/* XXX */
     }
@@ -1074,7 +1074,7 @@ run_exit_trap (void)
       else if (code == EXITPROG || code == EXITBLTIN)
 	retval = last_command_exit_value;
       else if (function_code != 0)
-        retval = return_catch_value;
+	retval = return_catch_value;
       else
 	retval = trap_saved_exit_value;
 
@@ -1195,12 +1195,12 @@ _run_trap_internal (int sig, char *tag)
       if (sig != DEBUG_TRAP && sig != RETURN_TRAP && sig != ERROR_TRAP)
 	flags |= SEVAL_RESETLINE;
       if (function_code == 0)
-        {
+	{
 	  parse_and_execute (trap_command, tag, flags);
 	  trap_exit_value = last_command_exit_value;
-        }
+	}
       else
-        trap_exit_value = return_catch_value;
+	trap_exit_value = return_catch_value;
       evalnest--;
 
 #if defined (JOB_CONTROL)
@@ -1553,7 +1553,7 @@ restore_traps (void)
 	    set_signal_handler (i, SIG_IGN);
 	}
       else if (trapstr != (char *)DEFAULT_SIG)
-        /* set_signal duplicates the string argument before freeing it. */
+	/* set_signal duplicates the string argument before freeing it. */
 	set_signal (i, trapstr);
 
       pending_traps[i] = 0;	/* XXX */
