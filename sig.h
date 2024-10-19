23a1ebe57c5093600f905d1759dd7bd84e5e53f7
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-19 17:37:01 +1000

    Reindent sig.h

--- sig.h
+++ sig.h
@@ -23,67 +23,67 @@
 #if !defined (_SIG_H_)
 #  define _SIG_H_
 
-#include "stdc.h"
+#  include "stdc.h"
 
-#include <signal.h>		/* for sig_atomic_t */
+#  include <signal.h>		/* for sig_atomic_t */
 
-#if !defined (SIGABRT) && defined (SIGIOT)
-#  define SIGABRT SIGIOT
-#endif
+#  if !defined (SIGABRT) && defined (SIGIOT)
+#    define SIGABRT SIGIOT
+#  endif
 
-#define sighandler void
+#  define sighandler void
 typedef void SigHandler (int);
 
-#define SIGRETURN(n)	return
+#  define SIGRETURN(n)	return
 
 /* Here is a definition for set_signal_handler () which simply expands to
    a call to signal () for non-Posix systems.  The code for set_signal_handler
    in the Posix case resides in general.c. */
-#if !defined (HAVE_POSIX_SIGNALS)
-#  define set_signal_handler(sig, handler) (SigHandler *)signal (sig, handler)
-#else
+#  if !defined (HAVE_POSIX_SIGNALS)
+#    define set_signal_handler(sig, handler) (SigHandler *)signal (sig, handler)
+#  else
 extern SigHandler *set_signal_handler (int, SigHandler *);	/* in sig.c */
-#endif /* _POSIX_VERSION */
+#  endif /* _POSIX_VERSION */
 
-#if !defined (SIGCHLD) && defined (SIGCLD)
-#  define SIGCHLD SIGCLD
-#endif
+#  if !defined (SIGCHLD) && defined (SIGCLD)
+#    define SIGCHLD SIGCLD
+#  endif
 
-#if !defined (HAVE_POSIX_SIGNALS) && !defined (sigmask)
-#  define sigmask(x) (1 << ((x)-1))
-#endif /* !HAVE_POSIX_SIGNALS && !sigmask */
+#  if !defined (HAVE_POSIX_SIGNALS) && !defined (sigmask)
+#    define sigmask(x) (1 << ((x)-1))
+#  endif /* !HAVE_POSIX_SIGNALS && !sigmask */
 
-#if !defined (HAVE_POSIX_SIGNALS)
-#  if !defined (SIG_BLOCK)
-#    define SIG_BLOCK 2
-#    define SIG_SETMASK 3
-#  endif /* SIG_BLOCK */
+#  if !defined (HAVE_POSIX_SIGNALS)
+#    if !defined (SIG_BLOCK)
+#      define SIG_BLOCK 2
+#      define SIG_SETMASK 3
+#    endif /* SIG_BLOCK */
 
 /* sigset_t defined in config.h */
 
 /* Make sure there is nothing inside the signal set. */
-#  define sigemptyset(set) (*(set) = 0)
+#    define sigemptyset(set) (*(set) = 0)
 
 /* Initialize the signal set to hold all signals. */
-#  define sigfillset(set) (*set) = sigmask (NSIG) - 1
+#    define sigfillset(set) (*set) = sigmask (NSIG) - 1
 
 /* Add SIG to the contents of SET. */
-#  define sigaddset(set, sig) *(set) |= sigmask (sig)
+#    define sigaddset(set, sig) *(set) |= sigmask (sig)
 
 /* Delete SIG from signal set SET. */
-#  define sigdelset(set, sig) *(set) &= ~sigmask (sig)
+#    define sigdelset(set, sig) *(set) &= ~sigmask (sig)
 
 /* Is SIG a member of the signal set SET? */
-#  define sigismember(set, sig) ((*(set) & sigmask (sig)) != 0)
+#    define sigismember(set, sig) ((*(set) & sigmask (sig)) != 0)
 
 /* Suspend the process until the reception of one of the signals
    not present in SET. */
-#  define sigsuspend(set) sigpause (*(set))
-#endif /* !HAVE_POSIX_SIGNALS */
+#    define sigsuspend(set) sigpause (*(set))
+#  endif /* !HAVE_POSIX_SIGNALS */
 
 /* These definitions are used both in POSIX and non-POSIX implementations. */
 
-#define BLOCK_SIGNAL(sig, nvar, ovar) \
+#  define BLOCK_SIGNAL(sig, nvar, ovar) \
 do { \
   sigemptyset (&nvar); \
   sigaddset (&nvar, sig); \
@@ -91,15 +91,15 @@ do { \
   sigprocmask (SIG_BLOCK, &nvar, &ovar); \
 } while (0)
 
-#define UNBLOCK_SIGNAL(ovar) sigprocmask (SIG_SETMASK, &ovar, (sigset_t *) NULL)
+#  define UNBLOCK_SIGNAL(ovar) sigprocmask (SIG_SETMASK, &ovar, (sigset_t *) NULL)
 
-#if defined (HAVE_POSIX_SIGNALS)
-#  define BLOCK_CHILD(nvar, ovar) BLOCK_SIGNAL (SIGCHLD, nvar, ovar)
-#  define UNBLOCK_CHILD(ovar) UNBLOCK_SIGNAL(ovar)
-#else /* !HAVE_POSIX_SIGNALS */
-#  define BLOCK_CHILD(nvar, ovar) ovar = sigblock (sigmask (SIGCHLD))
-#  define UNBLOCK_CHILD(ovar) sigsetmask (ovar)
-#endif /* !HAVE_POSIX_SIGNALS */
+#  if defined (HAVE_POSIX_SIGNALS)
+#    define BLOCK_CHILD(nvar, ovar) BLOCK_SIGNAL (SIGCHLD, nvar, ovar)
+#    define UNBLOCK_CHILD(ovar) UNBLOCK_SIGNAL(ovar)
+#  else /* !HAVE_POSIX_SIGNALS */
+#    define BLOCK_CHILD(nvar, ovar) ovar = sigblock (sigmask (SIGCHLD))
+#    define UNBLOCK_CHILD(ovar) sigsetmask (ovar)
+#  endif /* !HAVE_POSIX_SIGNALS */
 
 /* Extern variables */
 extern volatile sig_atomic_t sigwinch_received;
