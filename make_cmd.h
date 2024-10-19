44c021f2559086dd4fd72e3991bbc1e538b86625
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-19 15:27:37 +1000

    Reindent make_cmd.h

--- make_cmd.h
+++ make_cmd.h
@@ -19,9 +19,9 @@
 */
 
 #if !defined (_MAKE_CMD_H_)
-#define _MAKE_CMD_H_
+#  define _MAKE_CMD_H_
 
-#include "stdc.h"
+#  include "stdc.h"
 
 extern int here_doc_first_line;
 
@@ -35,7 +35,7 @@ extern WORD_DESC *make_word_from_token (int);
 
 extern WORD_LIST *make_word_list (WORD_DESC *, WORD_LIST *);
 
-#define add_string_to_list(s, l) make_word_list (make_word(s), (l))
+#  define add_string_to_list(s, l) make_word_list (make_word(s), (l))
 
 extern COMMAND *make_command (enum command_type, SIMPLE_COM *);
 extern COMMAND *command_connect (COMMAND *, COMMAND *, int);
@@ -57,10 +57,10 @@ extern COMMAND *make_arith_command (WORD_LIST *);
 
 extern COMMAND *make_select_command (WORD_DESC *, WORD_LIST *, COMMAND *, int);
 
-#if defined (COND_COMMAND)
+#  if defined (COND_COMMAND)
 extern COND_COM *make_cond_node (int, WORD_DESC *, COND_COM *, COND_COM *);
 extern COMMAND *make_cond_command (COND_COM *);
-#endif
+#  endif
 
 extern COMMAND *make_arith_for_command (WORD_LIST *, COMMAND *, int);
 
