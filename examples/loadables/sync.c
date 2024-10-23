e627f88b668a51095f9fec9954e5cd0e64f5261e
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-24 09:34:24 +1000

    char const * in examples/loadables/sync.c

--- examples/loadables/sync.c
+++ examples/loadables/sync.c
@@ -78,7 +78,7 @@ sync_builtin (WORD_LIST *list)
   return (status);
 }
 
-static char *sync_doc[] = {
+static char const *sync_doc[] = {
   "Sync disks or specified files.",
   "",
   "If one or more FILEs is supplied, force completion of pending writes",
