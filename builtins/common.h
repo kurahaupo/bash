bf76c79c75dc137b1f29cfe494ae384eb935afe0
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-24 12:47:29 +1000

    Get rid of shopt_login_shell (and thus set_login_shell)

--- builtins/common.h
+++ builtins/common.h
@@ -184,8 +184,6 @@ extern char **get_shopt_options (void);
 extern int shopt_setopt (char const *, int);
 extern int shopt_listopt (char const *, int);
 
-extern int set_login_shell (char *, int);
-
 extern void set_bashopts (void);
 extern void initialize_bashopts (int);
 
