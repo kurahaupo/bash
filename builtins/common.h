eaa5e263dcd82ac90f59ccb07fb5d8be94be0494
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-28 22:28:39 +1000

    Make parse_bashopts static in shopt.def

--- builtins/common.h
+++ builtins/common.h
@@ -181,8 +181,8 @@ extern void set_current_options (const char *);
 extern void reset_shopt_options (void);
 extern char **get_shopt_options (void);
 
-extern int shopt_setopt (char const*, int);
-extern int shopt_listopt (char const*, int);
+extern int shopt_setopt (char const *, int);
+extern int shopt_listopt (char const *, int);
 
 extern int set_login_shell (char *, int);
 
