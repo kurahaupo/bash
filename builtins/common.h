26557866766b1e9c2f3e5f9503df131fc43c822f
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-10-28 22:31:29 +1000

    const set_minus_o_option() in builtins/common.h

--- builtins/common.h
+++ builtins/common.h
@@ -166,7 +166,7 @@ extern void check_read_timeout (void);
 extern int minus_o_option_value (const char *);
 extern void list_minus_o_opts (int, int);
 extern char **get_minus_o_opts (void);
-extern int set_minus_o_option (char, char *);
+extern int set_minus_o_option (char, char const *);
 
 extern void set_shellopts (void);
 extern void parse_shellopts (char *);
