a5de2c73d85fbf4914b31a51b625cb0b6c6fae3d
Martin D Kealey <martin@kurahaupo.gen.nz>
2024-12-07 19:10:39 +1000

    EXTRACT THIS BUGFIX - make sure that termcap.a compiles without complaint

--- lib/termcap/termcap.c
+++ lib/termcap/termcap.c
@@ -113,8 +113,8 @@ int bufsize = 128;
 static void
 memory_out ()
 {
-  write (2, "virtual memory exhausted\n", 25);
-  exit (1);
+  _Bool ok = write (2, "virtual memory exhausted\n", 25) == 25;
+  exit (ok ? 1 : 2);
 }
 
 static char *
